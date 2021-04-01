927fe47422750ea6a7e95e78a3619020889cd6af
nicola <nicola@DESKTOP-9570QQL.localdomain>
2021-04-01 11:24:34 +0200

    Players Controller

---
+++ game/Assets/Scripts/Controller/Helpers/AnimateCameraLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCameraLevel : MonoBehaviour
+{
+    public float adjustSpeed = 15f;
+    private float _lerpFactor = 1f;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.localPosition;
+    }
+
+    public void UpdateLevel(float level, float crouchCamAdjust)
+    {
+        if (level == transform.localPosition.y)
+        {
+            _lerpFactor = 1f;
+            return;
+        }    
+        else
+            _lerpFactor = _lerpFactor == 1f ? 0f : Mathf.Min(1.0f, _lerpFactor + Time.deltaTime * adjustSpeed);
+
+        Vector3 crouchlevel = _offset;
+        crouchlevel.y += crouchCamAdjust;
+
+        if (level == crouchCamAdjust)    // crouching
+            transform.localPosition = Vector3.Lerp(_offset, crouchlevel, _lerpFactor);
+        else    // uncrouching
+            transform.localPosition = Vector3.Lerp(crouchlevel, _offset, _lerpFactor);
+    }
+}
---
+++ game/Assets/Scripts/Controller/MovementType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementType : MonoBehaviour
+{
+    public Status changeTo;
+
+    protected PlayerController player;
+    protected PlayerMovement movement;
+    protected PlayerInput playerInput;
+    protected Status playerStatus;
+
+    public virtual void Start()
+    {
+        player = GetComponent<PlayerController>();
+        player.AddMovementType(this);
+    }
+
+    public virtual void SetPlayerComponents(PlayerMovement move, PlayerInput input)
+    {
+        movement = move; playerInput = input;
+    }
+
+    public virtual void Movement()
+    {
+        //Movement info
+    }
+}
