e6f47323b15ee7732bf172c125071c61a4de33fb
nicola <nicola@DESKTOP-9570QQL.localdomain>
2021-04-19 22:35:26 +0200

    Match Timer + King Capture System

--- game/Assets/Scripts/General/GameManager.cs
+++ game/Assets/Scripts/General/GameManager.cs
@@ -31,12 +31,60 @@ public class GameManager : MonoBehaviour
 		DontDestroyOnLoad(gameObject);
 	}
 
+	// Public
+	[Tooltip("Expressed in seconds.")]
+	[SerializeField] private float _remainingMatchTime = 75f;
+	[SerializeField] private Transform _captureTransform = null;
+
+	// Internals
+	private bool _isGameOver = false;
+
 	// Every component in the scene has a unique id used as key of the dictionary
 	private Dictionary<int, PlayerInfo> _playersInfo = new Dictionary<int, PlayerInfo>();
 	private Dictionary<int, InteractiveItem> _interactiveItems = new Dictionary<int, InteractiveItem>();
 
 	// Properties
 	public Dictionary<int, PlayerInfo> PlayersInfo { get { return _playersInfo; } }
+
+	private void Update()
+	{
+		_remainingMatchTime -= Time.deltaTime;
+
+		if (_remainingMatchTime <= 0.0f)
+			_isGameOver = true;
+
+		//while (!_isGameOver)
+		//{
+		//	// do stuff
+		//}
+	}
+
+	// returns a properly formatted time string for the current match
+	public string FormatMatchTime()
+	{
+		if (_remainingMatchTime < 0) return null;
+
+		int minutes = Mathf.FloorToInt(_remainingMatchTime / 60.0f);
+		int seconds = Mathf.FloorToInt(_remainingMatchTime % 60);
+
+		string min = minutes >= 10 ? minutes.ToString() : "0" + minutes.ToString();
+		string sec = seconds >= 10 ? seconds.ToString() : "0" + seconds.ToString();
+
+		return min + " : " + sec;
+	}
+
+	// Process what should happen when the king captures a wizard.
+	public void CaptureWizard(CharacterManager charManager)
+	{
+		if (charManager == null)
+			return;
+
+		// TODO play some particle effects (smoke or something)
+
+		// teleport the wizard in a prison or something
+		charManager.transform.position = _captureTransform.position;
+		charManager.transform.rotation = _captureTransform.rotation;
+	}
 
 	// Register and Get a Player Info reference searched on by the instance ID of its collider
 	public void RegisterPlayerInfo(int key, PlayerInfo playerInfo)
