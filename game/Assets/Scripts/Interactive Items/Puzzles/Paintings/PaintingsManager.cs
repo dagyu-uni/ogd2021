79e36b325087338e08879af84854ff5458674b00
nicola <nicola@DESKTOP-9570QQL.localdomain>
2021-05-07 09:01:00 +0000

    [Bot] Automated dotnet-format update

--- game/Assets/Scripts/Interactive Items/Puzzles/Paintings/PaintingsManager.cs
+++ game/Assets/Scripts/Interactive Items/Puzzles/Paintings/PaintingsManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public enum ItemNames { Ring, Dice, GlobeMap, SkyrimMap};
+public enum ItemNames { Ring, Dice, GlobeMap, SkyrimMap };
 
 [System.Serializable]
 public class PaintingMaterials
