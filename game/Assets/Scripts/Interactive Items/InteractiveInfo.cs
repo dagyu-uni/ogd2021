9d78a63ca229c2209d2ffaa630e4a6b11a7cf43a
dagyu95 <dagyu95@gmail.com>
2021-06-10 15:36:58 +0200

    Some fix

--- game/Assets/Scripts/Interactive Items/InteractiveInfo.cs
+++ game/Assets/Scripts/Interactive Items/InteractiveInfo.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 // Interactive Item that simply shows some text in the HUD
 public class InteractiveInfo : InteractiveItem
-<<<<<<< HEAD
 {
 	public List<Role> roles = new List<Role>(new Role[] { Role.King, Role.Wizard_1, Role.Wizard_2 });
 
@@ -21,24 +20,6 @@ public class InteractiveInfo : InteractiveItem
 		}
 
 		return "";
-=======
-{
-	public List<Role> roles = new List<Role>();
-
-	// Inspector Assigned
-	[TextArea(3, 10)]
-	[SerializeField] private string _infoText = null;
-	[SerializeField] private GameObject _interfaceReadOnly = null;
-
-	public override string GetText(CharacterManager cm)
-	{
-		if (roles.Contains(cm.Role))
-		{
-			return _infoText;
-		}
-
-		return "";
->>>>>>> ec5d622868949af7303229f5fe1e8620266fbf97
 	}
 
 	public override void Activate(CharacterManager characterManager)
