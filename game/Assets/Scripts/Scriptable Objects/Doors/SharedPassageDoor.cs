be65ea9b0c8de46c23c2bf7f1a7329fb9077aa65
dagyu <dagyu95dev@gmail.com>
2021-06-23 17:57:51 +0200

    Fixed door and add artifacts

--- game/Assets/Scripts/Scriptable Objects/Doors/SharedPassageDoor.cs
+++ game/Assets/Scripts/Scriptable Objects/Doors/SharedPassageDoor.cs
@@ -5,23 +5,30 @@ public class SharedPassageDoor : Door
 {
 	public override void Interaction(InteractiveDoor interactiveDoor, CharacterManager cm)
 	{
-		string text = null;
+		string text = "";
 		if (cm.IsKing())
 		{
-			text = "Find a lever to open the door";
-		}
-		else if (cm.IsWizard() && cm.HasCollectable(CollectableName.Passpartout))
-		{
-			text = "You can open the door using a lever, find it.";
-		}
-		// wizards
-		else if (cm.IsWizard() && cm.HasCollectable(CollectableName.LockPick))
-		{
-			interactiveDoor.StartPuzzle(cm);
+			text = "Find a lever to "+ (interactiveDoor.isLocked ? "open" : "close") + " the door";
 		}
 		else if (cm.IsWizard())
 		{
-			text = "You need a lockpick to open the door";
+			if (cm.HasCollectable(CollectableName.Passpartout))
+			{
+				text = "You can open or close the door using a lever, find it.";
+			}
+			else if (interactiveDoor.isLocked && cm.HasCollectable(CollectableName.LockPick))
+			{
+				interactiveDoor.StartPuzzle(cm);
+			}
+			else if(interactiveDoor.isLocked)
+			{
+				text = "You need a lockpick to open the door";
+			}
+			else if (!interactiveDoor.isLocked)
+			{
+				text = "You can't close the door";
+			}
+
 		}
 		if (text != null)
 			cm.CallCoroutine(cm.PlayerHUD.SetEventText(text, cm.PlayerHUD.eventColors[0]));
