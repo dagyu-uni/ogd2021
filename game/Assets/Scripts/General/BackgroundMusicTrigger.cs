95df10b3aa3049fbdcbe42c49fe0736f640ab395
dagyu <dagyu95dev@gmail.com>
2021-06-27 21:12:07 +0200

    WIP

---
+++ game/Assets/Scripts/General/BackgroundMusicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicTrigger : MonoBehaviour
+{
+	[SerializeField] private AudioCollection _audioCollection = null;
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Play Music");
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Stop Music");
+	}
+}
