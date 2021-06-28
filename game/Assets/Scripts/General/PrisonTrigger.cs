8dc5dcdc1062e12b2bacddace3090c79f43b3305
nicola <nicola@DESKTOP-9570QQL.localdomain>
2021-06-28 03:59:36 +0200

    ..

--- game/Assets/Scripts/General/PrisonTrigger.cs
+++ game/Assets/Scripts/General/PrisonTrigger.cs
@@ -6,11 +6,19 @@ public class PrisonTrigger : MonoBehaviour
 {
 	[HideInInspector] public int numOfWizard = 0;
 
+	private List<int> _ids = new List<int>();
+
 	private void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.CompareTag("Wizard_1") || other.gameObject.CompareTag("Wizard_2"))
 		{
-			numOfWizard++;
+			int id = other.gameObject.GetInstanceID();
+
+			if (!_ids.Contains(id))
+			{
+				numOfWizard++;
+				_ids.Add(id);
+			}
 		}
 	}
 }
